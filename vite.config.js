bbb1203c67059da9b7f2b310ca38f901b1606e38
hasyimimah <114676690+hasyimimah@users.noreply.github.com>
2026-06-01 18:13:09 +0900

    Initialize Vite config for React application
    Add Vite configuration for React project with base path.

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // Menghindari masalah jalur aset (path) yang rusak di GitHub Pages
+})
